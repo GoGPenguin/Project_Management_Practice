25de21d9d9e271ab84a86dd84f9be576aaa6789d
PenguinBKDN <son72ltv2@gmail.com>
2024-01-20 00:08:42 +0700

    product client view

--- Controllers/Client/product.controller.js
+++ Controllers/Client/product.controller.js
@@ -1,5 +1,24 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model')
+
+module.exports.index = async (req, res) => {
+    const data = await Product.find({ 
+        status: "active",
+    });
+
+    const newProducts = data.map(item => {
+        item.price = (item.price * (100 - item.discountPercentage) / 100).toFixed(2)
+        return item
+        }
+    )    
+
+    data.forEach(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(2)
+    })
+
+    console.log(newProducts)
+
     res.render('Client/Pages/Products/index', {
         titlePage: "Danh sách sản phẩm",
+        data: data,
     })
 }
