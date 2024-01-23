a14e8618bd39d511f72f4645012cdf5f9a8502da
PenguinBKDN <son72ltv2@gmail.com>
2024-01-23 22:38:48 +0700

    general layout

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 
 const database = require('./config/database')
+const systemConfig = require('./config/system')
 
 const route = require("./routes/Client/index.route")
 const adminRoute = require('./Routes/admin/index.route')
@@ -20,6 +21,10 @@ app.use(express.static("public"))
 // Routes
 route(app)
 adminRoute(app)
+
+//Local Var
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
