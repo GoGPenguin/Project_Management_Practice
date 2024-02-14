823deb4aa44df15098f331ec99f5d62d6b601982
PenguinBKDN <son72ltv2@gmail.com>
2024-02-14 21:52:19 +0700

    feat: icon for chat feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 const formSendData = document.querySelector('.chat .inner-form')
 
 if (formSendData) {
@@ -19,16 +21,49 @@ socket.on('Server_return_message', (data) => {
     const body = document.querySelector('.chat .inner-body')
 
     const div = document.createElement('div')
-    
-    if (myId == data.userId) 
-        div.classList.add('inner-outgoing')
-    else
-        div.classList.add('inner-incoming')
 
-    div.innerHTML = `
+    if (myId == data.userId)
+        div.classList.add('inner-outgoing')
+    else {
+        div.classList.add('inner-incoming')
+        div.innerHTML = `
         <div class="inner-name">${data.fullName}</div>
+        `
+    }
+
+    div.innerHTML = div.innerHTML + `
         <div class="inner-content">${data.content}</div>
     `
 
     body.appendChild(div)
+    bodyChat.scrollTop = bodyChat.scrollHeight
 })
+
+const bodyChat = document.querySelector('.chat .inner-body')
+if (bodyChat) {
+    bodyChat.scrollTop = bodyChat.scrollHeight
+}
+
+
+
+const buttonIcon = document.querySelector('.button-icon')
+if (buttonIcon) {
+    const tooltip = document.querySelector('.tooltip')
+    Popper.createPopper(buttonIcon, tooltip)
+
+    buttonIcon.onclick = () => {
+        tooltip.classList.toggle('shown')
+    }
+}
+
+
+
+const emojiPicker = document.querySelector('emoji-picker')
+if (emojiPicker) {
+    const input = document.querySelector(".chat .inner-form input[name='content']")
+
+    emojiPicker.addEventListener('emoji-click', (event) => {
+        const icon = event.detail.unicode
+        input.value = input.value + icon
+    })
+}
