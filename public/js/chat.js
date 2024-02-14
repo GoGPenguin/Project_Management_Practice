979bba2ab7b9735c08fe8e26ed23f2535c2385eb
PenguinBKDN <son72ltv2@gmail.com>
2024-02-14 12:57:02 +0700

    fear: chat un-realtime

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-var socket = io()
+const formSendData = document.querySelector('.chat .inner-form')
+
+if (formSendData) {
+    formSendData.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const content = e.target.elements.content.value
+        
+        if(content) {
+            socket.emit('Client_send_message', content)
+            e.target.elements.content.value = ""
+        }
+    })
+}
+
